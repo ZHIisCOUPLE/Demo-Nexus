f9d702325307dfcc4f7af680cc01a2bd4d45c0cb
ZHIisCOUPLE <1796854053@qq.com>
2018-11-27 21:05:58 +0800

    演示如何使用Nexus就Maven私服

---
+++ NexusTest/Nexus-domai/src/main/java/com/test/domai/TestDemo.java
@@ -0,0 +1,9 @@
+package com.test.domai;
+
+import com.test.utils.TestUtils;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        TestUtils.showHi();
+    }
+}
---
+++ NexusTest/nexus-commens/src/main/java/com/test/utils/TestUtils.java
@@ -0,0 +1,9 @@
+package com.test.utils;
+
+public class TestUtils {
+
+    public static void showHi(){
+        System.out.println("hello  Nexus!!!!");
+    }
+
+}
